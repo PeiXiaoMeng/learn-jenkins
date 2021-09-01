3caf2631c91bf6ca740bb657d6cb43fa4c009cfe
PeiXiaoMeng <673427497@qq.com>
2021-09-01 17:56:17 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome to Your Vue.js App222222test第二版本"/>
   </div>
 </template>
 
